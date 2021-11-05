0f05063ff369821e98050a411d1787fbdb280c59
thinhtrai1 <ducthinhtrai@gmail.com>
2021-11-05 22:02:08 +0700

    Initial commit

--- src/main/kotlin/com/example/blog/BlogConfiguration.kt
+++ src/main/kotlin/com/example/blog/BlogConfiguration.kt
@@ -3,8 +3,10 @@ package com.example.blog
 import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("dev")
 class BlogConfiguration {
 
     @Bean
